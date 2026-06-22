5dc9f29db13a381d9a805fc1b7a88d111bf62ce5
Anny <eszwaq951@gmail.com>
2026-06-23 02:03:12 +0800

    測試正式環境判斷建立

---
+++ config.js
@@ -0,0 +1,15 @@
+window.APP_CONFIG = {
+  GOOGLE_API_KEY: 'AIzaSyB2IyH68acpouxGzSXHt-HjymBhTiH5WGk',
+  GOOGLE_CALENDAR_ID: '4ba0e8a1b31f1a821edc4aec49773111113ca7a0ab1080b4923500db0e382534@group.calendar.google.com',
+  TOKEN_COLLECTION: 'tokens',
+  FIREBASE_CONFIG: {
+    apiKey: 'AIzaSyB-zKGQ4NNNnRh-BMep97o66iSR7juKVoY',
+    authDomain: 'eson-unison-calendar-cd2f9.firebaseapp.com',
+    projectId: 'eson-unison-calendar-cd2f9',
+    storageBucket: 'eson-unison-calendar-cd2f9.firebasestorage.app',
+    messagingSenderId: '672800291334',
+    appId: '1:672800291334:web:818338c46b659531bfe187',
+    measurementId: 'G-XDER4CP7WP'
+  },
+  FIREBASE_VAPID_KEY: 'BNw9it2f72zhRwIbAJOpFtkqRJskjUBY8UlquoCbOR7b-jzfAU4ZwA6smGH2F273gHgj6DG1H5MCw5zve3V7kuM'
+};
